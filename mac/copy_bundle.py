a762addcafca4943a178671b0027eb1371c8110a
Carter Sande <carter.sande@duodecima.technology>
2019-11-17 22:28:16 -0800

    Get Pygame working (only build in the world with MP3 and video on Catalina!)

--- mac/copy_bundle.py
+++ mac/copy_bundle.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python2.7
 
 import os
+import os.path
 import subprocess
 import sys
 
 import macholib.MachO
 import macholib.util
 
-def copy_bundle(src, dest, src_prefix, dest_prefix):
-    subprocess.call(["rm", "-rf", dest])
-    subprocess.call(["ditto", src, dest])
-    subprocess.call(["find", dest, "-type", "l", "-delete"])
-    src_prefix = os.path.abspath(src_prefix)
+def copy_bundle(src, dest, src_prefixes, dest_prefix):
+    src_prefixes = list(map(os.path.abspath, src_prefixes))
     dest_prefix = os.path.abspath(dest_prefix)
-    for macho_file in macholib.util.iter_platform_files(dest):
+    if os.path.isfile(src):
+        files = [dest]
+        subprocess.call(["cp", src, dest])
+    else:
+        subprocess.call(["rm", "-rf", dest])
+        subprocess.call(["ditto", src, dest])
+        subprocess.call(["find", dest, "-type", "l", "-delete"])
+        files = macholib.util.iter_platform_files(dest)
+    for macho_file in files:
         print(macho_file)
         def changefunc(path):
-            if path.startswith(src_prefix):
-                return "@rpath" + path[len(src_prefix):]
-            elif path.startswith("@rpath"):
+            if path.startswith("@rpath"):
                 return path
+            for prefix in src_prefixes:
+                if path.startswith(prefix):
+                    return "@rpath" + path[len(prefix):]
         os.chmod(macho_file, 0755)
         macho = macholib.MachO.MachO(macho_file)
         rewroteAny = False
@@ -39,4 +46,4 @@ def copy_bundle(src, dest, src_prefix, dest_prefix):
             finally:
                 macholib.util.flipwritable(macho_file, old_mode)
 if __name__ == "__main__":
-    copy_bundle(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    copy_bundle(sys.argv[1], sys.argv[2], sys.argv[4:], sys.argv[3])
